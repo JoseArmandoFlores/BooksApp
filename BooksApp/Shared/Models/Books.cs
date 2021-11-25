d01555ab0672cdc9cc9239d4cbfc166bca984ae2
Jose Armando Flores Baldera <jose_flores3@ucne.edu.do>
2021-11-25 10:08:00 -0400

    Creating model

---
+++ BooksApp/Shared/Models/Books.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BooksApp.Shared.Models
+{
+    public class Books
+    {
+        [Required]
+        public int Id { get; set; }
+        [Required]
+        public string Title { get; set; }
+        [Required]
+        public string Description { get; set; }
+        [Required]
+        public int PageCount { get; set; }
+        [Required]
+        public string Excerpt { get; set; }
+        [Required]
+        public DateTime PublishDate { get; set; } = DateTime.Now;
+    }
+}
